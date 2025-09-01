a39aa87a3b1f070e4d009df39bb37b3a35f71676
timothynn <timothynn08@gmail.com>
2025-09-01 17:41:34 +0300

    cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
   outputs = { self, nixpkgs, home-manager, nixvim, catppuccin, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      pkgs = import nixpkgs { inherit system; };
     in
     {
       # --- NixOS system config ---
